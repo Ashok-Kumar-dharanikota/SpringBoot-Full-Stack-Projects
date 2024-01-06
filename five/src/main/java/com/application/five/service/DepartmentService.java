cafc35703ad1989f55e537cba144c71403f78ef3
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2024-01-06 14:04:42 +0530

    Service class for department

--- five/src/main/java/com/application/five/service/DepartmentService.java
+++ five/src/main/java/com/application/five/service/DepartmentService.java
@@ -1,10 +1,21 @@
 package com.application.five.service;
 
+import com.application.five.payload.DepartmentDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface DepartmentService {
 
+    public DepartmentDto createDepartment(DepartmentDto departmentDto);
 
+    public DepartmentDto getDepartment(long dept_Id);
+
+    public List<DepartmentDto> getAllDepartments();
+
+    public DepartmentDto updateDepartment(long dept_Id);
+
+    public void deleteDepartment(long dept_Id);
 
 }
